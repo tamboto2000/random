2fa13b19fd06b9cd58f767876593b3da4df84285
Franklin Collin Tamboto <tamboto2000@gmail.com>
2020-12-11 03:42:53 +0700

    Add new features

--- random.go
+++ random.go
@@ -1,18 +1,64 @@
+// Package random is a small library for generating random strings
 package random
 
 import (
+	"encoding/hex"
 	"math/rand"
 	"time"
 )
 
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+const (
+	lowerCase = "abcdefghijklmnopqrstuvwxyz"
+	upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	number    = "1234567890"
+	symbols   = `!@#$%^&*()_+{}|:"<>?-=[]\;',./`
+)
+
 const (
 	letterIdxBits = 6
 	letterIdxMask = 1<<letterIdxBits - 1
 	letterIdxMax  = 63 / letterIdxBits
 )
 
-func RandString(n int) string {
+// Option let you select additional letters to be added in generated random string
+type Option struct {
+	IncludeNumber    bool
+	IncludeUpperCase bool
+	IncludeSymbols   bool
+}
+
+// RandStr generate random string with length n, only use lower case latin alphabet
+func RandStr(n int) string {
+	return generateStr(n, Option{})
+}
+
+// RandStrWithOpt same as RandStr, but with option
+func RandStrWithOpt(n int, opt Option) string {
+	return generateStr(n, opt)
+}
+
+// RandHexStr generate random hexadecimals string with length n
+func RandHexStr(n int) string {
+	bytes := make([]byte, n)
+	rand.Read(bytes)
+
+	return hex.EncodeToString(bytes)
+}
+
+func generateStr(n int, opt Option) string {
+	letters := lowerCase
+	if opt.IncludeNumber {
+		letters += number
+	}
+
+	if opt.IncludeUpperCase {
+		letters += upperCase
+	}
+
+	if opt.IncludeSymbols {
+		letters += symbols
+	}
+
 	src := rand.NewSource(time.Now().UnixNano())
 
 	b := make([]byte, n)
@@ -20,8 +66,8 @@ func RandString(n int) string {
 		if remain == 0 {
 			cache, remain = src.Int63(), letterIdxMax
 		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
+		if idx := int(cache & letterIdxMask); idx < len(letters) {
+			b[i] = letters[idx]
 			i--
 		}
 		cache >>= letterIdxBits
